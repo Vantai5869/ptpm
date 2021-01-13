13d23ca5d6e72c5f06d84f2a9ef4ce9734052bbe
Hoàng Văn Tân <CT030248@actvn.edu.vn>
2021-01-13 18:23:47 +0700

    handle change and merge table

--- CongNghePhanMem/ViewModel/BookingViewModel.cs
+++ CongNghePhanMem/ViewModel/BookingViewModel.cs
@@ -17,12 +17,19 @@ namespace CongNghePhanMem.ViewModel
         public ICommand OrderCommand { get; set; }
         public ICommand PaymentCommand { get; set; }
         public ICommand LoadedWindowCommand { get; set; }
-
+        public ICommand ChangeTableCommand { get; set; }
+        public ICommand MergeTableCommand { get; set; }
+        static string table_id_get_1 = "";
+        static int dem = 0;
+        static string check = "";
         private ObservableCollection<TableModel> _List;
         public ObservableCollection<TableModel> List { get => _List; set { _List = value; OnPropertyChanged(); } }
         private TableModel _SelectedItem;
         private string _name;
         public string name { get => _name; set { _name = value; OnPropertyChanged(); } }
+        private string _id_select;
+        public string id_select { get => _id_select; set { _id_select = value; OnPropertyChanged(); } }
+
         public TableModel SelectedItem
         {
             get => _SelectedItem;
@@ -33,6 +40,19 @@ namespace CongNghePhanMem.ViewModel
                 if (SelectedItem != null)
                 {
                     name = SelectedItem.name;
+                    id_select = SelectedItem.id.ToString();
+                    if (table_id_get_1 != id_select && dem != 0)
+                    {
+                        dem = 0;
+                        if (check == "change")
+                        {
+                            ApiHandle.GetData(apiChangeTableUrl + table_id_get_1 + "/" + id_select);
+                        } else
+                        {
+                            ApiHandle.GetData(apiMergeTableUrl + table_id_get_1 + "/" + id_select);
+                        }
+                        listTable();
+                    }
                 }
             }
         }
@@ -73,6 +93,33 @@ namespace CongNghePhanMem.ViewModel
                 var id_table = SelectedItem.id.ToString();
                 var oderBill = new OderBill(apiTableBillUrl, id_table);
                 oderBill.Show();
+            });
+
+            ChangeTableCommand = new RelayCommand<object>((p) => {
+                if (SelectedItem == null)
+                    return false;
+                return true;
+            }, (p) =>
+            {
+                if (dem==0)
+                {
+                    check = "change";
+                    table_id_get_1 = id_select;
+                }
+                dem++;
+            });
+            MergeTableCommand = new RelayCommand<object>((p) => {
+                if (SelectedItem == null)
+                    return false;
+                return true;
+            }, (p) =>
+            {
+                if (dem == 0)
+                {
+                    check = "merge";
+                    table_id_get_1 = id_select;
+                }
+                dem++;
             });
         }
         void listTable()
